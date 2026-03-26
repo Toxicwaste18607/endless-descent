55ab49334a1d1e6f26699ab2c0cd61304dd2baf2
ToxicWaste18607 <Mielewski@outlook.com>
2026-03-26 23:10:59 +0000

    Mar 26, 2026, 7:10 PM

--- src/player.py
+++ src/player.py
@@ -26,7 +26,7 @@ class Player():
        self.draw_character(screen)
 
     def draw_health(self,screen):
-       healthbar=(20,10,200,20)
+       health_bar_=(20,10,200,20)
        healthbar_cover=()
        pygame.draw.rect(screen,(red),(healthbar) )
   
