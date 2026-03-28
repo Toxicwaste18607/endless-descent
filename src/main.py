7e3298aff159ec759109c22b552be90b0c497642
ToxicWaste18607 <Mielewski@outlook.com>
2026-03-28 22:33:20 +0000

    Mar 28, 2026, 6:33 PM

--- src/main.py
+++ src/main.py
@@ -117,7 +117,7 @@ clock = pygame.time.Clock()
 while running:
   #game code goes here
   floor_type_one()
-  screen.blit(floor_one)
+  screen.blit(floor_one, (screen))
   for wall in Walls.all_walls:
     wall.draw_wall(screen)
 
