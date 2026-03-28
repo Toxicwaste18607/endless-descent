b864024fc33fd5ac37d86b8aa44f25e0f59e0ad3
ToxicWaste18607 <Mielewski@outlook.com>
2026-03-28 23:21:40 +0000

    Mar 28, 2026, 7:21 PM

--- src/wall_class_code.py
+++ src/wall_class_code.py
@@ -13,12 +13,14 @@ class Walls():
     self.x_and_y=(self.x ,self.y)
     self.all_walls.append(self)
     self.wall_hitbox=pygame.Rect(self.x, self.y,self.width,self.height)
+    self.wall_vert=pygame.image.load("src/assets/wall_type_one.png").convert_alpha()
+    self.wall_vert=pygame.transform.scale(wall_vert,self.x_and_y)
+
 
 
 
   def draw_wall(self,screen):
-    wall_vert=pygame.image.load("src/assets/wall_type_one.png").convert_alpha()
-    wall_vert=pygame.transform.scale(wall_vert,self.x_and_y)
+
 
 
     screen.blit(wall_vert,self.x_and_y)
