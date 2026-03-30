a96e16baf9051b9c3fa8f35bc51808ced9a7ad28
ToxicWaste18607 <Mielewski@outlook.com>
2026-03-30 20:01:03 +0000

    Mar 30, 2026, 4:01 PM

--- src/player.py
+++ src/player.py
@@ -75,7 +75,7 @@ class Player():
       if self.hitbox.colliderect(other.hitbox):
          self.health-=0.1
       if self.health<=0:
-        pygame.draw.rect( screen, (blue), (self.hitbox ))
+        pygame.draw.rect( screen, (blue), (self.hitbox))
       
       
     def  sprint(self):
