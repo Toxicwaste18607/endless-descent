36f58120aeb5f86328efaf45bc1b3f2751514dfa
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-01 21:23:48 +0000

    Apr 1, 2026, 5:23 PM

--- src/wall_class_code.py
+++ src/wall_class_code.py
@@ -20,6 +20,7 @@ class Walls():
   def render_images(self):
     self.wall_vert=pygame.image.load("src/assets/walls_and_floors/wall_type_two_v.png").convert_alpha()
     self.wall_vert=pygame.transform.scale(self.wall_vert,(self.width,self.height))
+    
     self.wall_horl=pygame.image.load("src/assets/walls_and_floors/wall_type_two_h.png").convert_alpha()
     self.wall_horl=pygame.transform.scale(self.wall_horl,(self.width,self.height))
 
