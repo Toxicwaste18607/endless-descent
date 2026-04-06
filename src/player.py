061858c86d29c08fd94e3af87ee16c95eacda4d2
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-06 19:42:22 +0000

    Apr 6, 2026, 3:42 PM

--- src/player.py
+++ src/player.py
@@ -42,15 +42,19 @@ class Player():
        self.draw_stamina(screen)
 
     def draw_health(self,screen):
+      if self.height>0:
+        health_bar_hight= (self.health / self.max_health) * 200
+        health_bar_hight=abs(health_bar_hight)
+          
       
-      health_bar_hight= (self.health / self.max_health) * 200
-      health_bar_hight=abs(health_bar_hight)
-        
-    
-        
-      health_bar=(20,550,20,health_bar_hight)
-      healthbar_cover=()
-      pygame.draw.rect(screen,(red),(health_bar) )
+          
+        health_bar=(20,550,20,health_bar_hight)
+        healthbar_cover=()
+        pygame.draw.rect(screen,(red),(health_bar) )
+      
+      else:
+        pass
+
     
     def draw_stamina(self,screen):
       x= 1350
