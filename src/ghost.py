5543138c0b908a19026113753a2c56f84666e111
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-06 20:04:26 +0000

    Apr 6, 2026, 4:04 PM

--- src/ghost.py
+++ src/ghost.py
@@ -5,5 +5,9 @@ import pygame
 
 class Player_Ghost():
 
-    def __init__(self,other):
+    def __init__(self,x,y,width,hight):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.hight=hight
         pass
