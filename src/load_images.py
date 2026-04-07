dcd677fad831646bba29276c897c4c7c9a51307f
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-07 19:26:01 +0000

    Apr 7, 2026, 3:26 PM

--- src/load_images.py
+++ src/load_images.py
@@ -3,5 +3,9 @@ import pygame
 
 def load_images():
 
+  global floor_one
+  floor_one=pygame.image.load("src/assets/walls_and_floors/floor_type_one.png").convert_alpha()#this loads the floor png 
+  floor_one=pygame.transform.scale(floor_one, (screen_width,screen_height))
 
-    pass
+
+  
