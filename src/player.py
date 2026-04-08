4e4cb05f7b9c8f9459ba9e6ddc24beec995989a6
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-08 00:21:29 +0000

    Apr 7, 2026, 8:21 PM

--- src/player.py
+++ src/player.py
@@ -176,87 +176,65 @@ class Player():
 
 
 
-  def movement(player):
-    keys=pygame.key.get_pressed()
+  
+def movement(self):
+  keys=pygame.key.get_pressed()
 
-    walking=1
-    sprinting=2
-    speed = walking
-    if keys[pygame.K_LSHIFT]:
-      player.sprint()
-      
-      if player.sprint():
-        speed = sprinting 
-      else:
-        speed = walking
-
-
-
-
-
-
-    if keys[pygame.K_w]: #up
-      next_move=player.hitbox.copy()
-      next_move.y-=speed
-      if not collision(next_move): 
-          player.hitbox.y -= speed
-      
+  walking=1
+  sprinting=2
+  speed = walking
+  if keys[pygame.K_LSHIFT]:
+    self.sprint()
     
+    if self.sprint():
+      speed = sprinting 
+    else:
+      speed = walking
+
+  if keys[pygame.K_w]: #up
+    next_move=self.hitbox.copy()
+    next_move.y-=speed
+    if not collision(next_move): 
+        self.hitbox.y -= speed
     
-    if keys[pygame.K_s]:#down
-      next_move=player.hitbox.copy()
-      next_move.y+=speed
-      if not collision(next_move):
-          player.hitbox.y+=speed
-    
-      
-      
-
-    if keys[pygame.K_a]:#left
-      next_move=player.hitbox.copy()
-      next_move.x-=speed
-      if not collision(next_move):
-        player.hitbox.x-=speed
-
-
-    if keys[pygame.K_d]:#right
-      next_move=player.hitbox.copy()
-      next_move.x+=speed
-      if not collision(next_move):
-        player.hitbox.x+=speed
-    
-    if not keys[pygame.K_LSHIFT]:
-      player.stamina+=player.stamina_regain
-
+   
+  
+  if keys[pygame.K_s]:#down
+    next_move=self.hitbox.copy()
+    next_move.y+=speed
+    if not collision(next_move):
+        self.hitbox.y+=speed
+  
     
 
-    if player.hitbox.x < 0: #to stop the player from going off screen 
-      player.hitbox.x= 0 
-    
-    if player.hitbox.y<0:
-      player.hitbox.y=0
-
-    over_shoot_left= (screen_width - player.width)
-    if player.hitbox.x > over_shoot_left:
-      player.hitbox.x = over_shoot_left
-
-    over_shoot_down=(screen_height- player.height)
-    if player.hitbox.y> over_shoot_down:
-      player.hitbox.y = over_shoot_down
-
-  #test
+  if keys[pygame.K_a]:#left
+    next_move=self.hitbox.copy()
+    next_move.x-=speed
+    if not collision(next_move):
+      self.hitbox.x-=speed
 
 
+  if keys[pygame.K_d]:#right
+    next_move=self.hitbox.copy()
+    next_move.x+=speed
+    if not collision(next_move):
+      self.hitbox.x+=speed
+  
+  if not keys[pygame.K_LSHIFT]:
+    self.stamina+=self.stamina_regain
 
+  
 
-      
-            
-          
+  if self.hitbox.x < 0: #to stop the player from going off screen 
+    self.hitbox.x= 0 
+  
+  if self.hitbox.y<0:
+    self.hitbox.y=0
 
-        
-      
+  over_shoot_left= (screen_width - self.width)
+  if self.hitbox.x > over_shoot_left:
+    self.hitbox.x = over_shoot_left
 
-    
-
-
-
+  over_shoot_down=(screen_height- self.height)
+  if self.hitbox.y> over_shoot_down:
+    self.hitbox.y = over_shoot_down
