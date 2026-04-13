f4938618734dc2256050dafdd6727bd272cf993c
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-13 20:04:59 +0000

    Apr 13, 2026, 4:04 PM

--- dist/ghost.py
+++ dist/ghost.py
@@ -6,6 +6,6 @@ blue = (0, 0, 255)
 class Player_Ghost(Player):
     def __init__(self, hitbox):
         super().__init__(hitbox.x, hitbox.y)
-
+            
     def draw_character(self, screen):
         pygame.draw.rect(screen, blue, self.hitbox)
