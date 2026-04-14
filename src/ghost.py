91b7d55a0489644dcb5da29bdce55daf28065e77
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-14 21:30:49 +0000

    Apr 14, 2026, 5:30 PM

--- src/ghost.py
+++ src/ghost.py
@@ -1,21 +1,11 @@
 import pygame
+from player import *
 
-green =(0, 255, 0)
-blue=(0, 0, 255) 
-white=(255, 255, 255) 
-black=(0, 0, 0) 
-red=(255,0,0)
+blue = (0, 0, 255)
 
-
-class Player_ghost():
-
-    def __init__(self,screen,x,y,width,height):
-        self.x=x
-        self.y=y
-        self.width=width
-        self.hight=height
-        self.draw_ghost(screen)
-        pass
-
-    def draw_ghost(self,screen):
-        pygame.draw.rect( screen, (blue), (self.hitbox))
+class Player_Ghost(Player):
+    def __init__(self, hitbox):
+        super().__init__(hitbox.x, hitbox.y)
+            
+    def draw_character(self, screen):
+        pygame.draw.rect(screen, blue, self.hitbox)
