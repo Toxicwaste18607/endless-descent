bf3ef4347b1439bfcdd8f346179fcde69f6aec3e
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-14 23:55:33 +0000

    Apr 14, 2026, 7:55 PM

--- src/enemy_class.py
+++ src/enemy_class.py
@@ -16,21 +16,7 @@ red=(255,0,0)
 #this will be enemy base code used in all enemy classes
 class Enemy():
    all_enemies =[]
-   agro_range=300
-   
-   width= 50
-   height= 70
 
-   max_health=100
-   health=100
-   health_regen=1
-   attack_range = 5
-   damage=20
-   range_multi=2
-   stamina = 100
-   max_stamina=100
-   stamina_regain = .02
-   speed=1
    def __init__ (self ,x,y):
         self.hitbox=pygame.Rect(x, y, self.width, self.height)
         Enemy.all_enemies.append(self)
