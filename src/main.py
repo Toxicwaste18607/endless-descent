bfeb850dea3289765edc88607af37cda3448c83d
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-15 06:15:42 +0000

    Apr 15, 2026, 2:15 AM

--- src/main.py
+++ src/main.py
@@ -134,8 +134,8 @@ left= Walls(0, 0, 80, 800)
 right=Walls(1320, 0, 80, 80)
 
 # upper left room
-h1 = Walls(130, 360, 300, wall_thickness)
-v1 = Walls(450, 80, wall_thickness, 280)
+h1 = Walls(130, 350, 300, wall_thickness)
+v1 = Walls(390, 80, wall_thickness, 280)
 
 # upper middle room
 h2 = Walls(680, 430, 360, wall_thickness)
