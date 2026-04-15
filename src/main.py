06298c60e8b5122284208c82f10d55ee7384c597
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-15 07:51:26 +0000

    Apr 15, 2026, 3:51 AM

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ red=(255,0,0)
 
 def floor_type_one():
   global floor_one
-  floor_one=pygame.image.load("src/assets/walls_and_floors/floor_type.png").convert_alpha()#this loads the floor png 
+  floor_one=pygame.image.load("src/assets/walls_and_floors/floor_type_one.png").convert_alpha()#this loads the floor png 
   floor_one=pygame.transform.scale(floor_one, (screen_width,screen_height))
   
 
